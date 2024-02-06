4fdbb9cc9e423d49cc2bfe9030aee72724e1aa0e
idea <10103104+0x1DEA@users.noreply.github.com>
2024-02-06 01:48:11 -0600

    js build deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.js',
-            ssr: 'resources/js/ssr.js',
             refresh: true,
         }),
         vue({
@@ -18,4 +17,7 @@ export default defineConfig({
             },
         }),
     ],
+    ssr: {
+        noExternal: ['@inertiajs/server'],
+    },
 });
