4a601aaeae54e4886d11c8a630f178c158ec6f72
idea <10103104+0x1DEA@users.noreply.github.com>
2024-02-13 12:02:52 -0600

    hide routes

--- routes/web.php
+++ routes/web.php
@@ -48,86 +48,86 @@ Route::post('/chat/send', function (\Illuminate\Http\Request $request) {
     );
 })->middleware(['auth']);
 
-Route::get('/posts', function () {
-    $posts = \App\Models\Post::all()->load(['author']);
+//Route::get('/posts', function () {
+//    $posts = \App\Models\Post::all()->load(['author']);
+//
+//    return Inertia::render('_Posts', [
+//        'posts' => $posts,
+//        'club' => \App\Models\Club::query()->find(1)
+//    ]);
+//});
 
-    return Inertia::render('_Posts', [
-        'posts' => $posts,
-        'club' => \App\Models\Club::query()->find(1)
-    ]);
-});
+//Route::get('/join', function () {
+//    return Inertia::render('Join');
+//});
+//
+//Route::get('/mock', function () {
+//    return Inertia::render('Mock');
+//});
 
-Route::get('/join', function () {
-    return Inertia::render('Join');
-});
+//Route::get('/demos:new-club', function () {
+//    return Inertia::render('Demo/NewClub');
+//});
+//Route::post('/demos:new-club', function (\Illuminate\Http\Request $request) {
+//    $request->validate([
+//        'name' => 'required',
+//        'description' => 'required',
+//        'owner_id' => 'required',
+//    ]);
+//
+//    $club = new \App\Models\Club();
+//    $club->name = $request->string('name');
+//    $club->description = $request->string('description');
+//    $club->owner_id = $request->integer('owner_id');
+//    $club->save();
+//
+//    // unrelated but $request->fingerprint(); what is this?
+//
+//    return $club;
+//});
 
-Route::get('/mock', function () {
-    return Inertia::render('Mock');
-});
+//Route::get('/demos:new-post', function () {
+//    return Inertia::render('Demo/NewPost');
+//});
+//
+//Route::post('/demos:new-post', function (\Illuminate\Http\Request $request) {
+//    $request->validate([
+//        'name' => 'required',
+//        'description' => 'required',
+//        'owner_id' => 'required',
+//    ]);
+//
+//    $club = new \App\Models\Post();
+//    $club->title = $request->string('name');
+//    $club->slug = $request->string('slug');
+//    $club->content = $request->string('content');
+//    $club->author_id = $request->integer('author_id');
+//    $club->club_id = $request->integer('club_id');
+//    $club->published_at = $request->integer('published_at');
+//    $club->save();
+//
+//    // unrelated but $request->fingerprint(); what is this?
+//
+//    return $club;
+//});
 
-Route::get('/demos:new-club', function () {
-    return Inertia::render('Demo/NewClub');
-});
-Route::post('/demos:new-club', function (\Illuminate\Http\Request $request) {
-    $request->validate([
-        'name' => 'required',
-        'description' => 'required',
-        'owner_id' => 'required',
-    ]);
-
-    $club = new \App\Models\Club();
-    $club->name = $request->string('name');
-    $club->description = $request->string('description');
-    $club->owner_id = $request->integer('owner_id');
-    $club->save();
-
-    // unrelated but $request->fingerprint(); what is this?
-
-    return $club;
-});
-
-Route::get('/demos:new-post', function () {
-    return Inertia::render('Demo/NewPost');
-});
-
-Route::post('/demos:new-post', function (\Illuminate\Http\Request $request) {
-    $request->validate([
-        'name' => 'required',
-        'description' => 'required',
-        'owner_id' => 'required',
-    ]);
-
-    $club = new \App\Models\Post();
-    $club->title = $request->string('name');
-    $club->slug = $request->string('slug');
-    $club->content = $request->string('content');
-    $club->author_id = $request->integer('author_id');
-    $club->club_id = $request->integer('club_id');
-    $club->published_at = $request->integer('published_at');
-    $club->save();
-
-    // unrelated but $request->fingerprint(); what is this?
-
-    return $club;
-});
-
-Route::get('/generate', function () {
-
-    function g(): string
-    {
-        $a = rand(pow(10, 3), pow(10, 4) - 1);
-        $b = rand(pow(10, 3), pow(10, 4) - 1);
-        $c = rand(pow(10, 3), pow(10, 4) - 1);
-        return "MERE-${a}-${b}-${c}";
-    }
-
-    $codes = [];
-    for ($i = 0; $i < 200; $i++) {
-        $codes[] = g();
-    }
-
-    return $codes;
-});
+//Route::get('/generate', function () {
+//
+//    function g(): string
+//    {
+//        $a = rand(pow(10, 3), pow(10, 4) - 1);
+//        $b = rand(pow(10, 3), pow(10, 4) - 1);
+//        $c = rand(pow(10, 3), pow(10, 4) - 1);
+//        return "MERE-${a}-${b}-${c}";
+//    }
+//
+//    $codes = [];
+//    for ($i = 0; $i < 200; $i++) {
+//        $codes[] = g();
+//    }
+//
+//    return $codes;
+//});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
