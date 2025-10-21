5faba9b742e31c111ba00e2a78700d670a70227c
0x1DEA <seebeyondedm@gmail.com>
2025-10-21 18:34:16 -0500

    mobile carousel scrolling

--- resources/js/Components/Carousel.vue
+++ resources/js/Components/Carousel.vue
@@ -13,27 +13,41 @@ const page = ref(0);
 
 const pages = ref(slots.default()[0].children.length);
 
+const managedScroll = ref(false);
+
 const scroll = (amount = 1, behavior = 'smooth') => {
+    managedScroll.value = true;
     page.value += amount;
 
-    scroller.value.scrollTo({left: (scroller.value.clientWidth) * page.value, behavior: behavior})
+    scroller.value.scrollTo({left: scroller.value.clientWidth * page.value, behavior: behavior})
 }
 
 useResizeObserver(scroller, () => {
     scroll(0, 'auto')
 })
+
+const onScroll = () => {
+    // We're scrolling using the button, cancel this or else stuff will jitter and skip
+    if (managedScroll.value) return;
+    page.value = Math.round(scroller.value.scrollLeft / scroller.value.clientWidth);
+}
+
+const onScrollEnd = () => {
+    managedScroll.value = false
+    onScroll();
+}
 </script>
 <template>
     <div class="flex items-center relative">
-        <div v-if="page > 0" @click="scroll(-1)" class="absolute z-10 left-3 drop-shadow cursor-pointer text-white bg-neutral-700 rounded-full">
+        <div v-if="page > 0" @pointerdown="scroll(-1)" class="absolute z-10 left-3 drop-shadow cursor-pointer text-white bg-neutral-700 rounded-full">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6">
                 <path fill-rule="evenodd" d="M11.78 5.22a.75.75 0 0 1 0 1.06L8.06 10l3.72 3.72a.75.75 0 1 1-1.06 1.06l-4.25-4.25a.75.75 0 0 1 0-1.06l4.25-4.25a.75.75 0 0 1 1.06 0Z" clip-rule="evenodd" />
             </svg>
         </div>
-        <div ref="scroller" class="flex w-full items-start snap-x overflow-x-scroll no-scroller snap-mandatory">
+        <div ref="scroller" @scroll="onScroll" @scrollend="onScrollEnd" class="flex w-full items-start snap-x overflow-x-scroll no-scroller snap-mandatory relative z-0" :class="{'pointer-events-none': managedScroll}">
             <slot/>
         </div>
-        <div v-if="page < pages - 1" @click="scroll()" class="absolute z-10 right-3 drop-shadow cursor-pointer text-white bg-neutral-700 rounded-full">
+        <div v-if="page < pages - 1" @pointerdown="scroll()" class="absolute z-10 right-3 drop-shadow cursor-pointer text-white bg-neutral-700 rounded-full">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="size-6">
                 <path fill-rule="evenodd" d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
             </svg>
