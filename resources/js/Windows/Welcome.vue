e98d9c657558db5b243b7f532613a485e95242a6
0x1DEA <seebeyondedm@gmail.com>
2025-12-19 03:16:06 -0600

    add other account options to welcome window when logged in

--- resources/js/Windows/Welcome.vue
+++ resources/js/Windows/Welcome.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {isAuthenticated, openW} from "@/util.js";
+import {isAuthenticated, logout, openW} from "@/util.js";
 import Logo from "@/Components/Logo.vue";
 </script>
 <template>
@@ -7,5 +7,6 @@ import Logo from "@/Components/Logo.vue";
         <Logo/>
         <p class="text-center px-4 py-2">Welcome to rook.fm, your one stop shop for non-stop funk!<br/>Hang out in the chat, join a fan club, or just see what's on the radio.</p>
         <p v-if="!isAuthenticated()"><span @click="openW('Account')" class="underline">Log In</span></p>
+        <p v-else><span @click="openW('Account')" class="underline cursor-pointer">Account</span> | <span @click="logout" class="underline cursor-pointer">Log Out</span></p>
     </div>
 </template>
