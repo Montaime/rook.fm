b1c8bc69880eb3466f285854bb3126555ef616f1
0x1DEA <seebeyondedm@gmail.com>
2026-02-24 12:22:20 -0600

    Update dependencies and replace soketi broadcasting with reverb

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,21 +12,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
  * for events that are broadcast by Laravel. Echo and event broadcasting
- * allows your team to easily build robust real-time web applications.
+ * allow your team to quickly build robust real-time web applications.
  */
 
-import Echo from 'laravel-echo';
-
-import Pusher from 'pusher-js';
-window.Pusher = Pusher;
-
-window.Echo = new Echo({
-    broadcaster: 'pusher',
-    key: 'app-key',
-    cluster: '',
-    wsHost: 'rook.fm',
-    wsPort: 6001,
-    wssPort: 6002,
-    forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
-});
+import './echo';
